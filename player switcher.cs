49e8c17302391713b6d5e95dcecbeaef1e996a87
Imaekhai Oshiokhai <i.oshiokhai733@mybvc.ca>
2025-04-19 10:07:09 -0600

    Add files via upload
    Added PlayerSwitcher class to handle player turns

---
+++ player switcher.cs
@@ -0,0 +1,11 @@
+﻿public class PlayerSwitcher
+{
+    private string currentPlayer;
+    private string player1 = "Player 1";
+    private string player2 = "Player 2";
+
+    public void SwitchPlayer()
+    {
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
+    }
+}
